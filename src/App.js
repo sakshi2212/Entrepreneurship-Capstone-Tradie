f043ea0c39c1d4a8cdcb92da99bce85c45bc20a8
Sakshi Dhawan <dhawan.sakshi1222@gmail.com>
2024-10-06 21:48:02 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import TradingViewWidget from './TradingViewWidget';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>TradingView Financial Charts</h1>
+      {/* Example usage with different symbols */}
+      <TradingViewWidget symbol="NASDAQ:GOOGL" interval="D" theme="light" height={500} />
+      <TradingViewWidget symbol="NASDAQ:TSLA" interval="H" theme="dark" height={500} />
     </div>
   );
 }
