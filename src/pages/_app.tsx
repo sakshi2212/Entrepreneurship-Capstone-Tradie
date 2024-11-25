9b5eb650c7fc103b7017406068b096089c7af5d9
Sakshi Dhawan <dhawan.sakshi1222@gmail.com>
2024-11-25 00:06:00 -0500

    Final updated for chatbot and chart integration

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,3 @@
-// import { AppProps } from "next/app";
-// import { Toaster } from "@/components/ui/toaster";
-// import { PortfolioProvider } from "@/contexts/PortfolioContext";
-// import "@/styles/globals.css";
-
-// function MyApp({ Component, pageProps }: AppProps) {
-//   return (
-//     <PortfolioProvider>
-//       <Component {...pageProps} />
-//       <Toaster />
-//     </PortfolioProvider>
-//   );
-// }
-
-// export default MyApp;
-
 import { AppProps } from "next/app";
 import { Toaster } from "@/components/ui/toaster";
 import { PortfolioProvider } from "@/contexts/PortfolioContext";
